04b7c396cbb135eceb3d5fd19cbfbf227fc8c576
taka president <life.i.s.w---@i.softbank.jp>
2019-12-21 14:58:51 +0900

    Fix database.yml & Execute 'rails db:create'

--- db/migrate/20191221051803_create_lists.rb
+++ db/migrate/20191221051803_create_lists.rb
@@ -2,7 +2,7 @@ class CreateLists < ActiveRecord::Migration[5.2]
   def change
     create_table :lists do |t|
       t.string :description
-      t.boolean :completed
+      t.boolean :completed, default: false
 
       t.timestamps
     end
